6cd41ef883b09407277eb10d8fe533a3bb4d7ffe
Lil Momma <karyz230921@gmail.com>
2026-05-24 09:03:16 +0300

    Add Browser settings

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,16 +4,22 @@ import io.qameta.allure.Description;
 import io.qameta.allure.testng.AllureTestNg;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
-import pages.*;
+import pages.CartPage;
+import pages.CheckoutCompletePage;
+import pages.CheckoutInformationPage;
+import pages.CheckoutOverviewPage;
+import pages.LoginPage;
+import pages.ProductsPage;
 import utils.TestListener;
 
 import java.time.Duration;
 
-@Listeners ({AllureTestNg.class, TestListener.class})
+@Listeners({AllureTestNg.class, TestListener.class})
 public class BaseTest {
 
     protected WebDriver driver;
@@ -25,12 +31,21 @@ public class BaseTest {
     protected CheckoutOverviewPage checkoutOverviewPage;
     protected CheckoutCompletePage checkoutCompletePage;
 
-    @BeforeMethod (alwaysRun = true, description ="Настройка браузера")
-    @Description ("Настройка браузера")
+    @BeforeMethod(alwaysRun = true, description = "Настройка браузера")
+    @Description("Настройка браузера")
     public void setUp(ITestContext iTestContext) {
-        driver = new EdgeDriver();
+
+        EdgeOptions options = new EdgeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new EdgeDriver(options);
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
+
         // Page Objects
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
@@ -38,15 +53,16 @@ public class BaseTest {
         checkoutInformationPage = new CheckoutInformationPage(driver);
         checkoutOverviewPage = new CheckoutOverviewPage(driver);
         checkoutCompletePage = new CheckoutCompletePage(driver);
+
         iTestContext.setAttribute("driver", driver);
+
         // LOGIN
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
     }
 
-
-    @AfterMethod(alwaysRun = true,description = "Закрытие браузера" )
-    @Description ("Закрытие браузера")
+    @AfterMethod(alwaysRun = true, description = "Закрытие браузера")
+    @Description("Закрытие браузера")
     public void tearDown() {
         if (driver != null) {
             driver.quit();
